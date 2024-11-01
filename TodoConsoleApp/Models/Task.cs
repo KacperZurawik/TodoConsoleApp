1ec60a37051387f951b7bac16fd6fe5437245c3f
KacperZurawik <kaacper181@gmail.com>
2024-11-01 06:09:21 +0100

    Added constructor (that I forgot)

--- TodoConsoleApp/Models/Task.cs
+++ TodoConsoleApp/Models/Task.cs
@@ -11,5 +11,12 @@ namespace TodoConsoleApp.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-    }
+
+        public Task(int id, string title, string description)
+        {            
+            Id = id;
+            Title = title; 
+            Description = description;
+        }
+    }   
 }
