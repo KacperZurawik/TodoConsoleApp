a7ffc18c28dbffa7a2097303e808b795262e683d
KacperZurawik <kaacper181@gmail.com>
2024-11-01 06:10:18 +0100

    Added services folder and first method

---
+++ TodoConsoleApp/Services/TodoServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoConsoleApp.Services
+{
+    internal class TodoServices
+    {
+        public static void AddTask()
+        {
+
+            Console.Write("Title: ");
+            string title = Console.ReadLine();
+            Console.WriteLine("Description: ");
+            string description = Console.ReadLine();
+
+        }
+    }
+}
